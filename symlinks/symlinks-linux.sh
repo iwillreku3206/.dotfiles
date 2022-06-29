2a16b3880ee542e9d44b0c658ca8853bba8b24c4
IWillRekU3206 <iwillreku3206@gmail.com>
2022-06-29 12:59:59 +0800

    added i3 config

--- symlinks/symlinks-linux.sh
+++ symlinks/symlinks-linux.sh
@@ -1,1 +1,5 @@
-ln -sf .dotfiles/terminator ~/.config/terminator
+ln -sf ~/.dotfiles/config/terminator ~/.config/terminator
+ln -sf ~/.dotfiles/config/i3 ~/.config/i3
+ln -sf ~/.dotfiles/config/rofi ~/.config/rofi
+ln -sf ~/.dotfiles/config/polybar ~/.config/polybar
+ln -sf ~/.dotfiles/confid/picom.conf ~/.config/picom.conf
