80e63b590a8cf7a495abb38345c5f5da5b30c32e
IWillRekU3206 <iwillreku3206@gmail.com>
2022-06-29 13:01:50 +0800

    fixed type in picom config

--- symlinks/symlinks-linux.sh
+++ symlinks/symlinks-linux.sh
@@ -2,4 +2,4 @@ ln -sf ~/.dotfiles/config/terminator ~/.config/terminator
 ln -sf ~/.dotfiles/config/i3 ~/.config/i3
 ln -sf ~/.dotfiles/config/rofi ~/.config/rofi
 ln -sf ~/.dotfiles/config/polybar ~/.config/polybar
-ln -sf ~/.dotfiles/confid/picom.conf ~/.config/picom.conf
+ln -sf ~/.dotfiles/config/picom.conf ~/.config/picom.conf
