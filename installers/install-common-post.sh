cc0e3e8410a652fae2bb0d7182652db8c200aafd
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2022-08-09 00:12:47 +0800

    remove ycm setup from cfg installers, update neofetch cf

--- installers/install-common-post.sh
+++ installers/install-common-post.sh
@@ -3,8 +3,3 @@
 # symlink configs
 
 source '../symlinks/symlinks.sh'
-
-# install vim-ycm
-cd ~/.vim/pack/ycm/start/YouCompleteMe
-./install.py --all
-rm -rf ~/.dotfiles/.vim/pack/ycm/start/YouCompleteme/third_party/ycmd/third_party/tern_runtime
