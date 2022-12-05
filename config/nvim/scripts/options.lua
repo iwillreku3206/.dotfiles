93240e05aa3caa07828aa11616d9e4ea663e592a
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2022-12-05 22:33:03 +0800

    add prettier+eslint?

--- config/nvim/scripts/options.lua
+++ config/nvim/scripts/options.lua
@@ -5,3 +5,8 @@ vim.api.nvim_set_option('expandtab', true)
 vim.api.nvim_set_option('termguicolors', true)
 vim.api.nvim_set_option('title', true)
 vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = function()
+    vim.lsp.buf.format()
+  end
+})
--- config/nvim/scripts/plugins.lua
+++ config/nvim/scripts/plugins.lua
@@ -141,10 +141,33 @@ require('packer').startup(function(use)
         end
       end,
       })
-      end
+    end
   }
   use 'MunifTanjim/prettier.nvim'
-
+  use { 'MunifTanjim/eslint.nvim',
+    requires = {'jose-elias-alvarez/null-ls.nvim', 'MunifTanjim/eslint.nvim'},
+    config = function ()
+      require('eslint').setup({
+        bin = 'eslint', -- or `eslint_d`
+        code_actions = {
+          enable = true,
+          apply_on_save = {
+            enable = true,
+            types = { "directive", "problem", "suggestion", "layout" },
+          },
+          disable_rule_comment = {
+            enable = true,
+            location = "separate_line", -- or `same_line`
+          },
+        },
+        diagnostics = {
+          enable = true,
+          report_unused_disable_directives = false,
+          run_on = "type", -- or `save`
+        },
+      })
+    end
+  }
 
 
   -- Automatically set up your configuration after cloning packer.nvim
