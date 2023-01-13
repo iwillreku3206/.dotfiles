a5f92f9149f4186d7be6e2a10e98b45ef1477905
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-01-13 16:04:22 +0800

    modify cmp config

--- config/nvim/scripts/plugins.lua
+++ config/nvim/scripts/plugins.lua
@@ -99,7 +99,16 @@ require('packer').startup(function(use)
 
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
-  use 'hrsh7th/nvim-cmp'
+  use {
+    'hrsh7th/nvim-cmp',
+    config = function ()
+      require'cmp'.setup {
+        sources = {
+          { name = 'nvim_lsp_signature_help' }
+        }
+      }
+    end
+  }
   use 'hrsh7th/cmp-nvim-lsp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
