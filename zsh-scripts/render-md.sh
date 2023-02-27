9f87f6868d85357b30ef38d67efa367d8941b8b4
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-02-27 09:50:04 +0800

    dark magic

--- zsh-scripts/render-md.sh
+++ zsh-scripts/render-md.sh
@@ -13,5 +13,6 @@ function render-md {
     2=$1.pdf
   fi
   #sed "s/\[toc\]/\<nav\>\n\n\[toc\]\n\<\/nav\>/g" $1 | sed "s/\[toc\]/$(markdown-toc --bullets '1.' $1 | sed 's/$/\\n/g' | tr -d '\n')/g" | pandoc ${@:3} -s -f commonmark_x -o $2 --css=$(realpath ~/.dotfiles/pandoc/pandoc.css) --katex --pdf-engine=pagedjs-cli
-  sed "s/\[toc\]/\<nav\>\n\n\[toc\]\n\<\/nav\>\n\n/g" $1 | sed "s/\[toc\]/$(cat $1 | node ~/.dotfiles/pandoc/toc.js | sed 's/$/\\n/g' | tr -d '\n')/g" | pandoc ${@:3} -s -f markdown -o $2 --css=$(realpath ~/.dotfiles/pandoc/pandoc.css) --katex --pdf-engine=pagedjs-cli
+  #sed "s/\[toc\]/\<nav\>\n\n\[toc\]\n\<\/nav\>\n\n/g" $1 | sed "s/\[toc\]/$(cat $1 | node ~/.dotfiles/pandoc/toc.js | sed 's/$/\\n/g' | tr -d '\n')/g" | pandoc ${@:3} -s -f markdown -o $2 --css=$(realpath ~/.dotfiles/pandoc/pandoc.css) --katex --pdf-engine=pagedjs-cli
+  sed "s/\[toc\]/\<nav\>\n\n\[toc\]\n\<\/nav\>\n\n/g" $1 | sed "s/\[toc\]/$(cat $1 | node ~/.dotfiles/pandoc/toc.js | sed 's/$/\\n/g' | tr -d '\n')/g" | pandoc ${@:3} -s -f markdown -o $2 --css=$(realpath ~/.dotfiles/pandoc/pandoc.css) --katex  --pdf-engine=pagedjs-cli
 }
