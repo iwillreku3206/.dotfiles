6b600e7a8eab3728ac9bbd8f664ed5472fea2bc1
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-04-10 07:47:07 +0800

    add kitty to symlinks

--- symlinks/symlinks.sh
+++ symlinks/symlinks.sh
@@ -6,3 +6,4 @@ ln -sf ~/.dotfiles/.vscode ~/.vscode
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/.dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/config/kitty ~/.config/kitty
